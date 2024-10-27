d99a39f691147f11ba29212e011e7aede521b717
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-27 12:53:54 +0300

    feat(ui): add projects completed section.

--- app/(root)/our-projects/page.tsx
+++ app/(root)/our-projects/page.tsx
@@ -1,7 +1,12 @@
-import { heroOurProjects } from "@/constants";
+import { heroOurProjects, projects } from "@/constants";
 
 import { Hero } from "@/components/ui/hero";
 import { Heading } from "@/components/ui/heading";
+import Image from "next/image";
+import { Typography } from "@/components/ui/typography";
+import { CallOut } from "@/components/ui/call-out";
+import { Section } from "@/components/ui/section";
+import { format } from "date-fns";
 
 export default function ourProjectsPage() {
   return (
@@ -18,6 +23,57 @@ export default function ourProjectsPage() {
         pDescription="a brief explanation for Lorem ipsum dolit"
         lDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis."
       />
+      <Section id="papers" className="py-10">
+        <div className="container space-y-10 grid grid-cols-1 gap-20">
+          {projects.map((item, index) => (
+            <div key={index} className="space-y-10">
+              <CallOut variant="secondary" align="left">
+                <h3 className="text-mood-primary text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-sans font-bold uppercase">
+                  {item.title}
+                </h3>
+                <p className="text-mood-primary capitalize text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+                  {item.description}
+                </p>
+              </CallOut>
+              <Typography>{item.about}</Typography>
+              <div className="grid grid-cols-1 w-full gap-16">
+                {item.imgUrlBefore && (
+                  <div className="relative rounded-3xl overflow-hidden">
+                    <p className="absolute top-4 md:top-6 lg:top-8 right-4 md:right-6 lg:right-8 z-50 text-white font-bold text-base md:text-xl lg:text-2xl">
+                      {format(item.date, "dd")} | {format(item.date, "MM")} |
+                      {format(item.date, "yyyy")}
+                    </p>
+                    <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-[#39556E77] to-[#19344B11]"></div>
+                    <Image
+                      src={item.imgUrlBefore ?? ""}
+                      alt={`project-before-${index}`}
+                      className="object-cover w-full"
+                      width={1000}
+                      height={1000}
+                    />
+                  </div>
+                )}
+                {item.imgUrlAfter && (
+                  <div className="relative rounded-3xl overflow-hidden">
+                    <p className="absolute top-4 md:top-6 lg:top-8 right-4 md:right-6 lg:right-8 z-50 text-white font-bold text-base md:text-xl lg:text-2xl">
+                      {format(item.date, "dd")} | {format(item.date, "MM")} |{" "}
+                      {format(item.date, "yyyy")}
+                    </p>
+                    <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-[#39556E77] to-[#19344B11]"></div>
+                    <Image
+                      src={item.imgUrlAfter ?? ""}
+                      alt={`project-after-${index + 1}`}
+                      className="object-cover w-full"
+                      width={1000}
+                      height={1000}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </Section>
     </main>
   );
 }
--- constants/index.ts
+++ constants/index.ts
@@ -2,4 +2,57 @@ import heroAboutUs from "@/public/hero-about-us.png";
 import heroOurProjects from "@/public/hero-our-projects.png";
 import bgFooter from "@/public/bg-footer.png";
 
-export { heroAboutUs, heroOurProjects, bgFooter };
+const projects = [
+  {
+    id: 1,
+    title: "Shakhbout Villa 12",
+    description: "Design and build full interior",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
+    imgUrlBefore: "/project01_before.png",
+    imgUrlAfter: "/project01_after.png",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Shakhbout Villa 12",
+    description: "Design and build full interior",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
+    imgUrlBefore: null,
+    imgUrlAfter: "/project02_after.png",
+    date: new Date(),
+  },
+  {
+    id: 3,
+    title: "Shakhbout Villa 12",
+    description: "Design and build full interior",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
+    imgUrlBefore: null,
+    imgUrlAfter: "/project03_after.png",
+    date: new Date(),
+  },
+  {
+    id: 4,
+    title: "Shakhbout Villa 12",
+    description: "Design and build full interior",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
+    imgUrlBefore: "/project04_before.png",
+    imgUrlAfter: null,
+    date: new Date(),
+  },
+  {
+    id: 5,
+    title: "Shakhbout Villa 12",
+    description: "Design and build full interior",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
+    imgUrlBefore: "/project05_before.jpg",
+    imgUrlAfter: "/project05_after.jpg",
+    date: new Date(),
+  },
+];
+
+export { heroAboutUs, heroOurProjects, bgFooter, projects };
