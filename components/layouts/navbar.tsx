8e4086a9d16de2f683dcba63dd44a9e740373896
Saeed Altout <saeedaltoutpro@gmail.com>
2024-10-28 15:09:25 +0300

    refactor(ui): fixed navbar top and update link button.

--- components/layouts/navbar.tsx
+++ components/layouts/navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { Menu } from "lucide-react";
+import { MenuIcon } from "lucide-react";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
@@ -9,39 +10,47 @@ import { routes } from "@/config";
 
 import { Logo } from "@/components/ui/logo";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      setIsOpen(false);
+    }
+  };
 
   return (
-    <header className="bg-mood-primary">
+    <header className="bg-mood-primary fixed top-0 left-0 w-full z-50">
       <nav className="container mx-auto px-4 md:px-6 lg:px-8 flex justify-between items-center py-4">
         <Logo />
         <ul className="hidden lg:flex items-center justify-end gap-2">
           {routes.map((route) => (
-            <li
-              key={route.href}
-              className={cn(
-                "text-sm px-4 py-2 rounded-full w-full text-white hover:text-mood-primary hover:bg-mood-light transition-all text-nowrap",
-                pathname === route.href && "text-mood-primary bg-mood-light"
-              )}
-            >
-              <Link href={route.href}>{route.label}</Link>
+            <li key={route.href}>
+              <Link
+                href={route.href}
+                className={cn(
+                  "text-sm px-4 py-2 rounded-full w-full text-white hover:text-mood-primary hover:bg-mood-light transition-all text-nowrap",
+                  pathname === route.href && "text-mood-primary bg-mood-light"
+                )}
+              >
+                {route.label}
+              </Link>
             </li>
           ))}
         </ul>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="lg:hidden text-white hover:bg-transparent hover:text-white"
-            >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Menu button</span>
-            </Button>
-          </SheetTrigger>
+        <Sheet open={isOpen} onOpenChange={onChange}>
+          <Button
+            onClick={() => setIsOpen(true)}
+            size="icon"
+            variant="ghost"
+            className="lg:hidden text-white hover:bg-transparent hover:text-white"
+          >
+            <span className="sr-only">Menu button</span>
+            <MenuIcon className="h-6 w-6" />
+          </Button>
           <SheetContent className="space-y-8">
             <Logo className="ml-4 text-xl lg:text-3xl" />
             <ul className="flex flex-col items-start justify-start gap-4">
@@ -49,11 +58,17 @@ export const Navbar = () => {
                 <li
                   key={route.href}
                   className={cn(
-                    "text-sm px-4 py-2 rounded-md w-full text-white hover:text-mood-primary hover:bg-mood-light transition-all text-nowrap",
+                    "rounded-md w-full text-white hover:text-mood-primary hover:bg-mood-light transition-all",
                     pathname === route.href && "text-mood-primary bg-mood-light"
                   )}
+                  onClick={() => setIsOpen(false)}
                 >
-                  <Link href={route.href}>{route.label}</Link>
+                  <Link
+                    href={route.href}
+                    className="text-sm px-4 py-3 text-nowrap block"
+                  >
+                    {route.label}
+                  </Link>
                 </li>
               ))}
             </ul>
